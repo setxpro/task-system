904645af4b312e97847ef7cee21fab3b77a6a01d
Patrick Anjos <69186374+setxpro@users.noreply.github.com>
2023-06-22 09:59:41 -0300

    create Repositories and Intrefaces (User)

---
+++ SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+
+        Task<UsuarioModel> BuscarPorId(int Id);
+
+        Task<UsuarioModel> Adcionar(UsuarioModel usuario);
+
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int Id);
+
+        Task<bool> Apagar(int Id);
+    }
+}
